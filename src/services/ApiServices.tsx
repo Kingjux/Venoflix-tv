b6786a2aa5f8feab23468930f0ec3e4a66bff806
Kingjux <154667110+Kingjux@users.noreply.github.com>
2024-05-19 18:34:57 +0300

    Create ApiServices.tsx

---
+++ src/services/ApiServices.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { Anime, RecentAnime } from "../types/interface";
+
+const url = "https://mikiflix-api.vercel.app/meta/anilist/"
+
+async function fetchAnimeData(apiUrl: string, type: string) {
+  try {
+
+    const response = await fetch(apiUrl);
+
+    const data = await response.json()
+    
+    return data
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function getRecentAnime(): Promise<RecentAnime[]>{
+  const apiUrl = url + 'recent-episodes';
+  return await fetchAnimeData(apiUrl, 'general');
+}
+
+export async function getPopularAnime() : Promise<Anime[]> {
+  const apiUrl = url + 'popular';
+  return await fetchAnimeData(apiUrl, 'general');
+}
+
+export async function getDetailAnime(animeId: string | undefined) {
+  const apiUrl = url + 'info/' + animeId;
+  try {
+    const res = await fetchAnimeData(apiUrl, 'info')
+    console.log("API",res);
+  } catch (error) {
+    console.log("error : ",error);
+  }
+
+  return await fetchAnimeData(apiUrl, 'info');
+}
+export async function searchAnime(querry: string | undefined) {
+  const apiUrl = url + querry;
+  return await fetchAnimeData(apiUrl, 'general');
+}
